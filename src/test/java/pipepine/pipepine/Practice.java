a37c660d408504cc9382145b4e811ce624e71d93
mvipparthi <mvipparthi@levelsbeyond.com>
2020-09-10 23:19:38 +0530

    first commit

---
+++ src/test/java/pipepine/pipepine/Practice.java
@@ -0,0 +1,91 @@
+package pipepine.pipepine;
+
+import static org.testng.Assert.fail;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Practice {
+	private WebDriver driver;
+	private String baseUrl;
+	private boolean acceptNextAlert = true;
+	private StringBuffer verificationErrors = new StringBuffer();
+
+	@BeforeClass(alwaysRun = true)
+	public void setUp() throws Exception {
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//executables//chromedriver.exe");
+		driver = new ChromeDriver();
+		baseUrl = "https://www.techlistic.com/p/selenium-practice-form.html";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void testUntitledTestCase() throws Exception {
+		driver.get(baseUrl);
+		driver.findElement(By.name("firstname")).click();
+		driver.findElement(By.name("firstname")).clear();
+		driver.findElement(By.name("firstname")).sendKeys("Muneendra");
+		driver.findElement(By.name("lastname")).click();
+		driver.findElement(By.name("lastname")).clear();
+		driver.findElement(By.name("lastname")).sendKeys("vipparthi");
+		driver.findElement(By.id("sex-0")).click();
+		driver.findElement(By.id("exp-4")).click();
+		driver.findElement(By.id("datepicker")).click();
+		driver.findElement(By.id("datepicker")).clear();
+		driver.findElement(By.id("datepicker")).sendKeys("03/08/1992");
+		driver.findElement(By.id("profession-1")).click();
+		driver.findElement(By.id("tool-2")).click();
+		driver.findElement(By.id("tool-1")).click();
+	}
+
+	@AfterClass(alwaysRun = true)
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
+
+	private boolean isElementPresent(By by) {
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+
+	private boolean isAlertPresent() {
+		try {
+			driver.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
+
+	private String closeAlertAndGetItsText() {
+		try {
+			Alert alert = driver.switchTo().alert();
+			String alertText = alert.getText();
+			if (acceptNextAlert) {
+				alert.accept();
+			} else {
+				alert.dismiss();
+			}
+			return alertText;
+		} finally {
+			acceptNextAlert = true;
+		}
+	}
+}
